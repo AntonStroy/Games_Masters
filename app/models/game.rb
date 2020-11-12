0309693a9bb5b7453215dac3ac4cc6537147897d
Anton Stroy <antosha.stroy@gmail.com>
2020-11-12 15:07:04 -0600

    Added associations to all models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,13 @@
 class Game < ApplicationRecord
   belongs_to :developer
   belongs_to :publisher
+
+  has_many :genre_games
+  has_many :genres, through: :genre_games
+
+  has_many :platform_games
+  has_many :platforms, through: :platform_games
+
+  has_many :order_games
+  has_many :orders, through: :order_games
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :account
+  has_many :order_games
+  has_many :games, through: :order_games
 end
