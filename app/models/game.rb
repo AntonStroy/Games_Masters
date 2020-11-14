ff798a1fd209c53a43b44d71c1ec4be5103db9a3
Anton Stroy <antosha.stroy@gmail.com>
2020-11-13 23:41:35 -0600

    Added active addmin resources for accounts, games, genres, platforms and provinces.

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,9 +4,11 @@ class Game < ApplicationRecord
 
   has_many :genre_games
   has_many :genres, through: :genre_games
+  accepts_nested_attributes_for :genre_games, allow_destroy: true
 
   has_many :platform_games
   has_many :platforms, through: :platform_games
+  accepts_nested_attributes_for :platform_games, allow_destroy: true
 
   has_many :order_games
   has_many :orders, through: :order_games
