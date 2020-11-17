e871b63a4ddd7a9dd462bf4cf9158877660e082d
Anton Stroy <antosha.stroy@gmail.com>
2020-11-17 02:23:23 -0600

    Marage branches

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-const { environment } = require('@rails/webpacker')
-
-module.exports = environment
-=======
 const { environment } = require('@rails/webpacker')
 const jquery = require('./plugins/jquery')
 
 environment.plugins.prepend('jquery', jquery)
 module.exports = environment
->>>>>>> active_admin
