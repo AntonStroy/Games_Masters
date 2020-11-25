06b35a76fef1cdaeb153b50c775252a331747282
Anton Stroy <antosha.stroy@gmail.com>
2020-11-24 19:48:20 -0600

    Added cart controller, added initialize_session method and a cart method to the apllication controller. added before_action and cart helper method to the application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,19 @@ class ApplicationController < ActionController::Base
   def cms_pages
     Page.all
   end
+
+  before_action :initialize_session
+  helper_method :cart
+
+  private
+
+  def initialize_session
+    # This will be where we initialize our shopping cart.
+    session[:shopping_cart] ||= [] # the shopping cart will be an array of product ids.
+  end
+
+  def cart
+    # Return a collection of Product objects based on the product ids in the shopping cart.
+    Product.find(session[:shopping_cart])
+  end
 end
