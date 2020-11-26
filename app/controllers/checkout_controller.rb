462312c85ea5b9daf1ea7f5690681bd6325ec8a5
Anton Stroy <antosha.stroy@gmail.com>
2020-11-25 18:52:03 -0600

    Added code to the create method, added create javascript file, add a buy button to the game show view. add a reference link to the js. stripe library.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,30 @@
 class CheckoutController < ApplicationController
-  def create; end
+  def create
+    @game = Game.find(params[:id])
+
+    if @game.nil?
+      redirect_to root_path
+      return
+    end
+
+    # Setting up a Stripe Session for payment.
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ["card"],
+      line_items:           [{
+        name:        @game.name,
+        description: @game.description,
+        amount:      (@game.price * 100).to_i,
+        currency:    "cad",
+        quantity:    1
+      }],
+      success_url:          checkout_success_url,
+      cancel_url:           checkout_cancel_url
+    )
+
+    respond_to do |format|
+      format.js # render create.js.erb
+    end
+  end
 
   def success; end
 
